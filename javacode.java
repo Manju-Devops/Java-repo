9876e50fab1686c93d9ff156912fd8a54169c6b7
Manju-Devops <gowdamanju250@gmail.com>
2019-11-15 21:06:04 +0530

    5th commit

--- javacode.java
+++ javacode.java
@@ -1,6 +1,6 @@
 class Simple{
 public static void main(String args[]) throws InterruptedException{
 Thread.sleep(10000);
-System.out.println("Hello Java,Waiting fro auto poll from github trigers from Jenkin job");
+System.out.println("Hello Java,Waiting job trigger from Jenkin job");
 }
 }
